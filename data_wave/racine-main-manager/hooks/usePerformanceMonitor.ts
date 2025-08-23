3dd6143649906661c60b1579119f413a1547d7b9
seiforesti <148006804+seiforesti@users.noreply.github.com>
2025-08-23 19:13:28 +0100

    Delete data_wave/racine-main-manager directory

--- data_wave/racine-main-manager/hooks/usePerformanceMonitor.ts
+++
@@ -1,6 +0,0 @@
-// hooks/usePerformanceMonitor.ts
-'use client'
-
-import { usePerformanceMonitoring } from '../hooks/usePerformanceMonitoring'
-
-export const usePerformanceMonitor = usePerformanceMonitoring
